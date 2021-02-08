4e42ab5e9670bd8e39f53576b771d849ac70092f
William Jensen <williambjensen01@gmail.com>
2021-02-07 19:02:04 -0700

    fixed the project_completed

--- api/project/model.js
+++ api/project/model.js
@@ -2,12 +2,21 @@
 const db = require('../../data/dbConfig');
 
 async function getProjects() {
-    return db("projects");
+    const projects = await db("projects");
+    return projects.map((project) => {
+      return {
+        ...project,
+        project_completed: project.project_completed == 0 ? false : true,
+      };
+    });
 }
 
 async function getProjectById(id) {
     const [project] = await db("projects").where({ id });
-    return project;
+    return {
+      ...project,
+      project_completed: project.project_completed == 0 ? false : true,
+    };
 }
 
 async function addProject(data) {
